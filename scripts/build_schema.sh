44b3306f0a799ba5ee78d4998424234a79fbe046
BarnacleBull <129243792+BarnacleBull@users.noreply.github.com>
2023-04-02 23:30:57 -0400

    Deploy 1 local pool (#1)
    * add schema and artifacts
    
    * build_release for any platform
    
    * install sei/cosmwasm packages
    
    * wallet creation
    
    * update performTransaction
    
    * upload pair contracts
    
    * deploy router
    
    * deploy local pools

--- scripts/build_schema.sh
+++ scripts/build_schema.sh
@@ -6,15 +6,9 @@ set -o pipefail
 projectPath=$(cd "$(dirname "${0}")" && cd ../ && pwd)
 
 for c in "$projectPath"/contracts/*; do
-  if [[ "$c" != *"tokenomics" ]]; then
-    if [[ "$c" != *"periphery" ]]; then
-      (cd $c && cargo schema)
-    fi
+  if [[ "$c" != *"periphery" ]]; then
+    (cd $c && cargo schema)
   fi
-done
-
-for c in "$projectPath"/contracts/tokenomics/*; do
-  (cd $c && cargo schema)
 done
 
 for c in "$projectPath"/contracts/periphery/*; do
--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -1,195 +1,146 @@
 import 'dotenv/config'
+
 import {
-    Coin,
-    Coins,
-    isTxError,
-    LCDClient,
-    LocalTerra,
-    MnemonicKey,
-    Msg,
-    MsgExecuteContract,
-    MsgInstantiateContract,
-    MsgMigrateContract,
-    MsgStoreCode,
-    MsgUpdateContractAdmin, Tx,
-    Wallet
-} from '@terra-money/terra.js';
-import {
-    readFileSync,
-    writeFileSync,
+  readFileSync,
+  writeFileSync,
 } from 'fs'
 import path from 'path'
 import { CustomError } from 'ts-custom-error'
+import fs from 'fs'
+import https from 'https'
 
-import { APIParams } from "@terra-money/terra.js/dist/client/lcd/APIRequester";
-import fs from "fs";
-import https from "https";
+import {
+  AccountData,
+  Coin,
+  coin,
+  DirectSecp256k1HdWallet,
+  EncodeObject
+} from '@cosmjs/proto-signing'
+import { getSigningCosmWasmClient } from '@sei-js/core'
+import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
+import {calculateFee, GasPrice, isDeliverTxFailure} from '@cosmjs/stargate'
 
 export const ARTIFACTS_PATH = '../artifacts'
 
-export function getRemoteFile(file: any, url: any) {
-    let localFile = fs.createWriteStream(path.join(ARTIFACTS_PATH, `${file}.json`));
+export interface Wallet {
+  account: AccountData
+  chainId: string
+  client: SigningCosmWasmClient
+  gasPrice: GasPrice
+}
+export class TransactionError extends CustomError {
+  public constructor(
+    public code: string | number,
+    public transactionHash: string | undefined,
+    public rawLog: string | undefined,
+  ) {
+    super("transaction failed")
+  }
+}
 
-    https.get(url, (res) => {
-        res.pipe(localFile);
-        res.on("finish", () => {
-            file.close();
-        })
-    }).on('error', (e) => {
-        console.error(e);
-    });
+export function getRemoteFile(file: any, url: any) {
+  const localFile = fs.createWriteStream(path.join(ARTIFACTS_PATH, `${file}.json`));
+
+  https.get(url, (res) => {
+    res.pipe(localFile);
+    res.on("finish", () => {
+      file.close();
+    })
+  }).on('error', (e) => {
+    console.error(e);
+  });
 }
 
 export function readArtifact(name: string = 'artifact', from: string = ARTIFACTS_PATH) {
-    try {
-        const data = readFileSync(path.join(from, `${name}.json`), 'utf8')
-        return JSON.parse(data)
-    } catch (e) {
-        return {}
-    }
+  try {
+    const data = readFileSync(path.join(from, `${name}.json`), 'utf8')
+    return JSON.parse(data)
+  } catch (e) {
+    return {}
+  }
 }
 
-export interface Client {
-    wallet: Wallet
-    terra: LCDClient | LocalTerra
-}
+export async function newWallet(): Promise<Wallet> {
+  if (!process.env.MNEMONIC) {
+    throw new Error('Set the MNEMONIC env variable to the mnemonic of the wallet to use')
+  }
 
-export function newClient(): Client {
-    const client = <Client>{}
-    if (process.env.WALLET) {
-        client.terra = new LCDClient({
-            URL: String(process.env.LCD_CLIENT_URL),
-            chainID: String(process.env.CHAIN_ID)
-        })
-        client.wallet = recover(client.terra, process.env.WALLET)
-    } else {
-        client.terra = new LocalTerra()
-        client.wallet = (client.terra as LocalTerra).wallets.test1
-    }
-    return client
+  if (!process.env.GAS_PRICE) {
+    throw new Error('Set the GAS_PRICE env variable to the gas price to use when creating client')
+  }
+
+  if (!process.env.RPC_URL) {
+    throw new Error('Set the RPC_URL env variable to the RPC URL of the node to use')
+  }
+
+  const signer = await DirectSecp256k1HdWallet.fromMnemonic(process.env.MNEMONIC, {
+    prefix: 'sei'
+  })
+
+  const accounts = await signer.getAccounts()
+  if (accounts.length === 0) {
+    throw new Error('No accounts found in wallet')
+  }
+
+  if (accounts.length > 1) {
+    throw new Error('Multiple accounts found in wallet. Not sure which to use')
+  }
+
+  const account = accounts[0]
+  const gasPrice = GasPrice.fromString(process.env.GAS_PRICE)
+
+  const client = await getSigningCosmWasmClient(process.env.RPC_URL, signer, {
+    gasPrice: gasPrice
+  })
+  const chainId = await client.getChainId()
+
+  if (chainId !== process.env.CHAIN_ID) {
+    throw new Error(`Chain ID mismatch. Expected ${process.env.CHAIN_ID}, got ${chainId}`)
+  }
+
+  return { account, chainId, client, gasPrice }
 }
 
 export function writeArtifact(data: object, name: string = 'artifact', to: string = ARTIFACTS_PATH) {
-    writeFileSync(path.join(to, `${name}.json`), JSON.stringify(data, null, 2))
-}
-
-// Tequila lcd is load balanced, so txs can't be sent too fast, otherwise account sequence queries
-// may resolve an older state depending on which lcd you end up with. Generally 1000 ms is enough
-// for all nodes to sync up.
-let TIMEOUT = 1000
-
-export function setTimeoutDuration(t: number) {
-    TIMEOUT = t
-}
-
-export function getTimeoutDuration() {
-    return TIMEOUT
+  writeFileSync(path.join(to, `${name}.json`), JSON.stringify(data, null, 2))
 }
 
 export async function sleep(timeout: number) {
-    await new Promise(resolve => setTimeout(resolve, timeout))
+  await new Promise(resolve => setTimeout(resolve, timeout))
 }
 
-export class TransactionError extends CustomError {
-    public constructor(
-        public code: string | number,
-        public txhash: string | undefined,
-        public rawLog: string,
-    ) {
-        super("transaction failed")
-    }
+export async function performTransaction(wallet: Wallet, msg: EncodeObject) {
+  const result = await wallet.client.signAndBroadcast(wallet.account.address, [msg], calculateFee(4000000, wallet.gasPrice))
+  if (isDeliverTxFailure(result)) {
+    throw new TransactionError(result.code, result.transactionHash, result.rawLog)
+  }
+  return result
 }
 
-export async function createTransaction(wallet: Wallet, msg: Msg) {
-    return await wallet.createAndSignTx({ msgs: [msg] })
+export async function uploadContract(wallet: Wallet, filepath: string) {
+  const contract = readFileSync(filepath)
+  const result = await wallet.client.upload(wallet.account.address, contract, calculateFee(4000000, wallet.gasPrice))
+  return result.codeId
 }
 
-export async function broadcastTransaction(terra: LCDClient, signedTx: Tx) {
-    const result = await terra.tx.broadcast(signedTx)
-    await sleep(TIMEOUT)
-    return result
+export async function instantiateContract(wallet: Wallet, admin_address: string | undefined, codeId: number, msg: object, label: string) {
+  const result = await wallet.client.instantiate(wallet.account.address, codeId, msg, label, calculateFee(4000000, wallet.gasPrice), {
+    admin: admin_address
+  })
+  return result.logs[0].events.filter(el => el.type == 'instantiate').map(x => x.attributes.filter(element => element.key == '_contract_address').map(x => x.value));
 }
 
-export async function performTransaction(terra: LCDClient, wallet: Wallet, msg: Msg) {
-    const signedTx = await createTransaction(wallet, msg)
-    const result = await broadcastTransaction(terra, signedTx)
-    if (isTxError(result)) {
-        throw new TransactionError(result.code, result.codespace, result.raw_log)
-    }
-    return result
+export async function executeContract(wallet: Wallet, contractAddress: string, msg: object, funds?: Coin[]) {
+    return await wallet.client.execute(wallet.account.address, contractAddress, msg, calculateFee(4000000, wallet.gasPrice), undefined, funds)
 }
 
-export async function uploadContract(terra: LCDClient, wallet: Wallet, filepath: string) {
-    const contract = readFileSync(filepath, 'base64');
-    const uploadMsg = new MsgStoreCode(wallet.key.accAddress, contract);
-    let result = await performTransaction(terra, wallet, uploadMsg);
-    return Number(result.logs[0].eventsByType.store_code.code_id[0]) // code_id
+export async function queryContract(wallet: Wallet, contractAddress: string, query: object): Promise<any> {
+  return await wallet.client.queryContractSmart(contractAddress, query)
 }
 
-export async function instantiateContract(terra: LCDClient, wallet: Wallet, admin_address: string | undefined, codeId: number, msg: object, label: string) {
-    const instantiateMsg = new MsgInstantiateContract(wallet.key.accAddress, admin_address, codeId, msg, undefined, label);
-    let result = await performTransaction(terra, wallet, instantiateMsg)
-    return result.logs[0].events.filter(el => el.type == 'instantiate').map(x => x.attributes.filter(element => element.key == '_contract_address').map(x => x.value));
-}
-
-export async function executeContract(terra: LCDClient, wallet: Wallet, contractAddress: string, msg: object, coins?: Coins.Input) {
-    const executeMsg = new MsgExecuteContract(wallet.key.accAddress, contractAddress, msg, coins);
-    return await performTransaction(terra, wallet, executeMsg);
-}
-
-export async function queryContract(terra: LCDClient, contractAddress: string, query: object): Promise<any> {
-    return await terra.wasm.contractQuery(contractAddress, query)
-}
-
-export async function queryContractInfo(terra: LCDClient, contractAddress: string): Promise<any> {
-    return await terra.wasm.contractInfo(contractAddress)
-}
-
-export async function queryCodeInfo(terra: LCDClient, codeID: number): Promise<any> {
-    return await terra.wasm.codeInfo(codeID)
-}
-
-export async function queryContractRaw(terra: LCDClient, end_point: string, params?: APIParams): Promise<any> {
-    return await terra.apiRequester.getRaw(end_point, params)
-}
-
-export async function deployContract(terra: LCDClient, wallet: Wallet, admin_address: string, filepath: string, initMsg: object, label: string) {
-    const codeId = await uploadContract(terra, wallet, filepath);
-    return await instantiateContract(terra, wallet, admin_address, codeId, initMsg, label);
-}
-
-export async function migrate(terra: LCDClient, wallet: Wallet, contractAddress: string, newCodeId: number, msg: object) {
-    const migrateMsg = new MsgMigrateContract(wallet.key.accAddress, contractAddress, newCodeId, msg);
-    return await performTransaction(terra, wallet, migrateMsg);
-}
-
-export function recover(terra: LCDClient, mnemonic: string) {
-    const mk = new MnemonicKey({ mnemonic: mnemonic });
-    return terra.wallet(mk);
-}
-
-export async function update_contract_admin(
-    terra: LCDClient,
-    wallet: Wallet,
-    contract_address: string,
-    admin_address: string
-) {
-    let msg = new MsgUpdateContractAdmin(
-        wallet.key.accAddress,
-        admin_address,
-        contract_address
-    );
-
-    return await performTransaction(terra, wallet, msg);
-}
-
-export function initialize(terra: LCDClient) {
-    const mk = new MnemonicKey();
-
-    console.log(`Account Address: ${mk.accAddress}`);
-    console.log(`MnemonicKey: ${mk.mnemonic}`);
-
-    return terra.wallet(mk);
+export async function deployContract(wallet: Wallet, admin_address: string, filepath: string, initMsg: object, label: string) {
+  const codeId = await uploadContract(wallet, filepath);
+  return await instantiateContract(wallet, admin_address, codeId, initMsg, label);
 }
 
 export function toEncodedBinary(object: any) {
@@ -233,7 +184,7 @@ export class NativeAsset {
     }
 
     toCoin() {
-        return new Coin(this.denom, this.amount || "0")
+        return coin(this.amount || "0", this.denom)
     }
 }
 
